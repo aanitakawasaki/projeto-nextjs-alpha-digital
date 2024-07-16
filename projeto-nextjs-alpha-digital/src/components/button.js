8f1a05f1c77e2eb010e74dfebd87cd2deeb5eb4a
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-07-16 11:39:07 -0300

    entendendo button.js

--- projeto-nextjs-alpha-digital/src/components/button.js
+++ projeto-nextjs-alpha-digital/src/components/button.js
@@ -1,11 +1,13 @@
-const Button = ({ onClick, children, disabled }) => (
+const Button = ({ children, disabled }) => (
   <button 
-    onClick={onClick}
     className={`button ${disabled ? 'button-disabled' : ''}`} 
     disabled={disabled}
   >
     {children}
   </button>
 );
+//button é sempre aplicada, fornecendo os estilos básicos do botão. disabled ? 'button-disabled' : '': Aplica a classe button-disabled se disabled for true. Caso contrário, uma string vazia é aplicada.
+//disabled={disabled}: Define o atributo disabled do elemento <button> com base na prop disabled (que está definida lá em index.js).
+//{children}: Renderiza o conteúdo passado entre as tags de abertura e fechamento do componente Button.
 
 export default Button;
